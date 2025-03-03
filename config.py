423da95f30839252aa989f2a4080c4d2689e8ab5
denjieditor <denjieditor669@gmail.com>
2025-03-03 23:44:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 que = {}
 admins = {}
 SESSION_NAME = getenv("SESSION_NAME", "BQGPeTAAoSBZE9dECVktyt8bRaRyIJ3MaWTPOclLsT263H3eclVDVLaMT9kJb2xloH2NfXQBmfGEpQRnLIHUftTgTUlYSmT_Kaur30uXbPnINGitt3Vm7NRtz2lD79Mnq6nXmIn_H_N8FApYy3TlMNrt3BofFc9ujb3kLMx3yvt2L_wnRA_yAnVkHxboAGx1ZaWp29AQVtzeFxNGhq82tiMx8lordxpUoTKsIAks-aGz3WEU6ezwSs1y2QLnZKhyZ3qOHUuB4_9c_GBEAWUM2oOPGz5LEO6L-w6y4oDVBc37hg11Ljp8SoLD37Vyf-VSX6G_L1AL_wTEO7odoob8tLixoH0oEwAAAAHMd5hzAQ")
-BOT_TOKEN = getenv("7725357171:AAEM1T16TbfLJ0zYYBUNqPWLRBojDOdu_VU")
+BOT_TOKEN = getenv("7725357171:AAFSOBIGkh4uywQmagnr_89xaErKOTpfoow")
 BOT_NAME = getenv("BOT_NAME", "🆁🅸🅺🅰乂𝓜𝓾𝓼𝓲𝓬🎵")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/6790864f5fe27471bdc8d.png")
 THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/e9a4d6655e5ddf51f9160.jpg")
@@ -25,9 +25,9 @@ OWNER_NAME = getenv("OWNER_NAME", "@denji_god) # isi dengan username kamu tanpa 
 PMPERMIT = getenv("PMPERMIT", None)
 OWNER_ID = int(os.environ.get("5478596071")) # fill with your id as the owner of the bot
 DATABASE_URL = os.environ.get("mongodb+srv://denji:@denji.0qi3h.mongodb.net/?retryWrites=true&w=majority&appName=denji") # fill with your mongodb url
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) # make a private channel and get the channel id
+LOG_CHANNEL = int(os.environ.get("-1002304722288")) # make a private channel and get the channel id
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)) # just fill with True or False (optional)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+DURATION_LIMIT = int(getenv("0.0.0.0", "60"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-LANG = getenv("LANG", "id")
+LANG = getenv("all", "42408")
